f86accf0eb9215694ca4958e34eecc5979694bcc
saw <lawdrel@gmail.com>
2018-05-15 15:38:06 +0300

    add files

---
+++ widgets/Buttontest.php
@@ -0,0 +1,56 @@
+<?php
+
+
+
+namespace lawsaw\widgets;
+use Yii;
+use yii\base\Widget;
+
+
+
+class Buttontest extends Widget {
+
+    public $theme;
+    public $size;
+    public $iconBeforeClass;
+    public $iconBefore;
+    public $iconAfterClass;
+    public $iconAfter;
+    public $href;
+    public $label;
+    public $className;
+    public $classContent;
+    public $classTitle;
+    public $classText;
+    public $modal;
+    public $model;
+    public $onclick;
+
+
+    public function init() {
+        parent::init();
+    }
+
+    public function run() {
+        $options = [
+            'theme' => $this->theme,
+            'size' => $this->size,
+            'iconBeforeClass' => $this->iconBeforeClass,
+            'iconBefore' => $this->iconBefore,
+            'iconAfterClass' => $this->iconAfterClass,
+            'iconAfter' => $this->iconAfter,
+            'href' => $this->href,
+            'label' => $this->label,
+            'className' => $this->className,
+            'classContent' => $this->classContent,
+            'classTitle' => $this->classTitle,
+            'classText' => $this->classText,
+            'modal' => $this->modal,
+            'model' => $this->model,
+            'onclick' => $this->onclick,
+        ];
+        return $this->render('buttontest', $options);
+    }
+}
+
+?>
---
+++ widgets/views/buttontest.php
@@ -0,0 +1,115 @@
+<div class="buttonAwesome <?= $size ? 'buttonAwesome--'.$size : '' ?> <?= $theme ? 'buttonAwesome--'.$theme : '' ?> <?= $className ? $className : '' ?>">
+
+
+
+    <<?= $href ? 'a href="'.$href.'"' : 'div' ?>
+    class="buttonAwesome-content <?= $classContent ? $classContent : '' ?>"
+
+    <?= isset($modal) ? isset($modal['id']) ? 'data-awesome-modal="'.$modal['id'].'"' : '' : '' ?>
+    <?= isset($modal) ? isset($modal['mode']) ? 'data-awesome-modal-mode="'.$modal['mode'].'"' : '' : '' ?>
+    <?= isset($modal) ? isset($modal['anim']) ? isset($modal['anim']['in']) ? 'data-awesome-modal-anim-in="'.$modal['anim']['in'].'"' : '' : '' : '' ?>
+    <?= isset($modal) ? isset($modal['anim']) ? isset($modal['anim']['out']) ? 'data-awesome-modal-anim-out="'.$modal['anim']['out'].'"' : '' : '' : '' ?>
+    <?= isset($modal) ? isset($modal['workClass']) ? 'data-awesome-modal-workClass="'.$modal['workClass'].'"' : '' : '' ?>
+    <?= isset($modal) ? isset($modal['winClass']) ? 'data-awesome-modal-winClass="'.$modal['winClass'].'"' : '' : '' ?>
+    <?= isset($modal) ? isset($modal['layout']) ? 'data-awesome-modal-layout="'.$modal['layout'].'"' : '' : '' ?>
+    <?= isset($modal) ? isset($modal['model']) ? 'data-awesome-modal-model="'.$modal['model'].'"' : '' : '' ?>
+
+    <?= isset($onclick) ? 'onclick='.$onclick : '' ?>
+
+
+
+    >
+
+
+
+
+
+    <?php
+    if($iconBefore) {
+        ?>
+        <div class="buttonAwesome-icon buttonAwesome-icon--before <?= $iconBeforeClass ? $iconBeforeClass : '' ?>">
+            <?php
+            switch($iconBefore['type']) {
+                case 'img':
+                    ?>
+                    <img src="<?= $iconBefore['value'] ?>" />
+                    <?php
+                    break;
+                case 'svg':
+
+                    break;
+                case 'fa':
+                    ?>
+                    <i class="fa <?= $iconBefore['value'] ?>"></i>
+                    <?php
+                    break;
+                case 'block':
+                    ?>
+                    <div class="buttonAwesome-icon-image <?= $iconBefore['value'] ?>"></div>
+                    <?php
+                    break;
+                    ?>
+                    <?php
+            }
+            ?>
+        </div>
+        <?php
+    }
+    ?>
+
+
+
+    <?php
+    if($label) {
+        ?>
+        <div class="buttonAwesome-title <?= $classTitle ? $classTitle : '' ?>">
+            <div class="buttonAwesome-text <?= $classText ? $classText : '' ?>">
+                <?= $label ?>
+            </div>
+        </div>
+        <?php
+    }
+    ?>
+
+
+
+    <?php
+    if($iconAfter) {
+        ?>
+        <div class="buttonAwesome-icon buttonAwesome-icon--after <?= $iconAfterClass ? $iconAfterClass : '' ?>">
+            <?php
+            switch($iconAfter['type']) {
+                case 'img':
+                    ?>
+                    <img src="<?= $iconAfter['value'] ?>" />
+                    <?php
+                    break;
+                case 'svg':
+                  
+                    break;
+                case 'fa':
+                    ?>
+                    <i class="fa <?= $iconAfter['value'] ?>"></i>
+                    <?php
+                    break;
+                case 'block':
+                    ?>
+                    <div class="buttonAwesome-icon-image <?= $iconAfter['value'] ?>"></div>
+                    <?php
+                    break;
+                    ?>
+                    <?php
+            }
+            ?>
+        </div>
+        <?php
+    }
+    ?>
+
+
+
+</<?= $href ? 'a' : 'div' ?>>
+
+
+
+</div>
