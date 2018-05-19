23fee919f8ff5e2f00208dfd71cdb197588c7894
Saw <lawdrel@gmail.com>
2018-05-19 19:56:28 +0300

    add files

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -6,10 +6,6 @@ class Bootstrap implements BootstrapInterface{
     //Метод, который вызывается автоматически при каждом запросе
     public function bootstrap($app)
     {
-        //Правила маршрутизации
-        $app->getUrlManager()->addRules([
-            'test' => 'mytest/test/index',
-        ], false);
         /*
          * Регистрация модуля в приложении
          * (вместо указания в файле frontend/config/main.php
